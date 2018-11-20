a268582fcb319b3fc0bb08584129e4899cb0c179
JieTi <JieTi@LAPTOP-JIE-TIAN>
2018-11-20 17:25:31 +0800

    fix掉网页调用错误后无法发报警的错误

--- Aurora.Jobs/Aurora.Jobs/Core/SchedulerJobListener.cs
+++ Aurora.Jobs/Aurora.Jobs/Core/SchedulerJobListener.cs
@@ -25,49 +25,56 @@ namespace Aurora.Jobs.Core
 
         public Task JobWasExecuted(IJobExecutionContext context, JobExecutionException jobException, CancellationToken cancellationToken = default(CancellationToken))
         {
-            _logger.Debug($"SchedulerJobListener.JobWasExecuted: {context.JobDetail.Key.Name}");
-            var taskId = int.Parse(context.JobDetail.Key.Name);
-            DateTime nextFireTimeUtc = TimeZoneInfo.ConvertTimeFromUtc(context.NextFireTimeUtc.Value.DateTime, TimeZoneInfo.Local);
-            DateTime fireTimeUtc = TimeZoneInfo.ConvertTimeFromUtc(context.FireTimeUtc.UtcDateTime, TimeZoneInfo.Local);
-
-            double duration = context.JobRunTime.TotalSeconds;
-            string jobName = string.Empty;
-            string executedResult = string.Empty;
-            if (context.MergedJobDataMap != null)
+            try
             {
-                _logger.Debug($"SchedulerJobListener.JobWasExecuted context.MergedJobDataMap: {context.MergedJobDataMap.Count}");
+                _logger.Debug($"SchedulerJobListener.JobWasExecuted: {context.JobDetail.Key.Name}");
+                var taskId = int.Parse(context.JobDetail.Key.Name);
+                DateTime nextFireTimeUtc = TimeZoneInfo.ConvertTimeFromUtc(context.NextFireTimeUtc.Value.DateTime, TimeZoneInfo.Local);
+                DateTime fireTimeUtc = TimeZoneInfo.ConvertTimeFromUtc(context.FireTimeUtc.UtcDateTime, TimeZoneInfo.Local);
 
-                jobName = context.MergedJobDataMap.GetString("JobName");
-                executedResult = context.MergedJobDataMap.FirstOrDefault(a => a.Key == "executedResult").Value as string;
-                //foreach (var item in context.MergedJobDataMap)
-                //{
-                //    string key = item.Key;
-                //    if (key=="executedResult")
-                //    {
-                //        if (i > 0)
-                //        {
-                //            log.Append(",");
-                //        }
-                //        log.Append(item.Value);
-                //        i++;
-                //    }
-                //}
-                //if (i > 0)
-                //{
-                //    executedResult = string.Concat(log.ToString());
-                //}
+                double duration = context.JobRunTime.TotalSeconds;
+                string jobName = string.Empty;
+                string executedResult = string.Empty;
+                if (context.MergedJobDataMap != null)
+                {
+                    _logger.Debug($"SchedulerJobListener.JobWasExecuted context.MergedJobDataMap: {context.MergedJobDataMap.Count}");
 
-                _logger.Debug($"SchedulerJobListener.JobWasExecuted: {executedResult}");
+                    jobName = context.MergedJobDataMap.GetString("JobName");
+                    executedResult = context.MergedJobDataMap.FirstOrDefault(a => a.Key == "executedResult").Value as string;
+                    //foreach (var item in context.MergedJobDataMap)
+                    //{
+                    //    string key = item.Key;
+                    //    if (key=="executedResult")
+                    //    {
+                    //        if (i > 0)
+                    //        {
+                    //            log.Append(",");
+                    //        }
+                    //        log.Append(item.Value);
+                    //        i++;
+                    //    }
+                    //}
+                    //if (i > 0)
+                    //{
+                    //    executedResult = string.Concat(log.ToString());
+                    //}
+
+                    _logger.Debug($"SchedulerJobListener.JobWasExecuted: {executedResult}");
+
+                }
+                if (jobException != null)
+                {
+                    executedResult = executedResult + " EX:" + jobException.ToString();
+                }
+
+                executedResult = executedResult.Length > 1000 ? executedResult.Substring(0, 1000) : executedResult;
+                new ScheduledTaskService().UpdateScheduledTaskStatus(taskId, jobName, fireTimeUtc, nextFireTimeUtc, duration, executedResult);
+            }
+            catch(Exception ex)
+            {
+                _logger.Error(ex.Message);
 
             }
-            if (jobException != null)
-            {
-                executedResult = executedResult + " EX:" + jobException.ToString();
-            }
-
-            executedResult = executedResult.Length > 1000 ? executedResult.Substring(0, 1000) : executedResult;
-            new ScheduledTaskService().UpdateScheduledTaskStatus(taskId, jobName, fireTimeUtc, nextFireTimeUtc, duration, executedResult);
-
             return Task.FromResult(true);
         }
 
